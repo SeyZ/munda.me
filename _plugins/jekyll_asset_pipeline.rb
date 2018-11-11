93587b7ede79b7d95dcb7e41f044ec6486e731c9
Sandro Munda <sandro@munda.me>
2018-11-11 15:32:07 +0100

    Compress CSS

--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -13,15 +13,26 @@ module JekyllAssetPipeline
     end
   end
 
+  class CssCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.css'
+    end
+
+    def compress
+      return YUI::CssCompressor.new.compress(@content)
+    end
+  end
+
   class JavaScriptCompressor < JekyllAssetPipeline::Compressor
     def self.filetype
       '.js'
     end
 
     def compress
-        return @content
+      return @content
     end
   end
 end
-
 
